ffbc836bc9d0511ce7dd618702bc9b91afdfc491
sberdine77 <sbsx77xavier@hotmail.com>
2018-06-19 18:56:20 -0300

    Fixing json serialization issues. Using library to retrieve image from url and store on cash memory

--- desafioIOS/desafioIOS/ViewController.swift
+++ desafioIOS/desafioIOS/ViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 import Alamofire
+import Kingfisher
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var data: [(String, UIImage)] = []
+    var data: [(String, UIImageView)] = []
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        data = [("One", UIImage(named: "cat")!), ("Two", UIImage(named: "cat")!), ("Three", UIImage(named: "cat")!)]
+//        data = [("One", UIImage(named: "cat")!), ("Two", UIImage(named: "cat")!), ("Three", UIImage(named: "cat")!)]
         self.tableView.delegate = self
         self.tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,9 +28,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             "size": "3"
         ]
         
-        Alamofire.request("https://desafio-mobile-pitang.herokuapp.com/movies/list?", method: .get, parameters: parameters, headers: headers).responseJSON { (response) in
+        Alamofire.request("https://desafio-mobile-pitang.herokuapp.com/movies/list?", method: .get, parameters: parameters, headers: headers).validate().responseJSON { (response) in
             print("AQUI")
             print(response)
+            
+            guard response.result.isSuccess else {
+                print("Error while fetching: \(String(describing: response.result.error))")
+                return
+            }
+            
+            guard let value = response.result.value as? [[String: Any]] else {
+                print("Malformed data recived")
+                return
+            }
+            
+            for movie in value {
+                var element: (String, UIImageView)!
+                if let name = movie["name"] as? String {
+                    element.0 = name
+                }
+                if let urlImage = movie["url"] as? String {
+                    let url = URL(string: urlImage)!
+                    element.1.kf.setImage(with: url)
+                }
+            }
+            
         }
         
     }
