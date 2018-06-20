48335bbc4562ae8fef0864f49cb564b06587ff01
Pedro Veloso <pedrofveloso@gmail.com>
2018-06-20 12:25:20 -0300

    adding xcode project to repository

---
+++ Pitang - Desafio iOS/Pitang - Desafio iOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Pitang - Desafio iOS
+//
+//  Created by Pedro Veloso on 20/06/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
